7c4b599e48587eb2dc56f113f0ecbef3ff164ebb
andrewjpage <andrewjpage@gmail.com>
2012-07-24 11:15:32 +0100

    parallelise download

--- modules/MLST/Download/Databases.pm
+++ modules/MLST/Download/Databases.pm
@@ -15,22 +15,29 @@ $databases->update;
 package MLST::Download::Databases;
 use Moose;
 use MLST::Download::Database;
+use Parallel::ForkManager;
 
 has 'databases_attributes' => ( is => 'ro', isa => 'HashRef', required => 1 );
 has 'base_directory'       => ( is => 'ro', isa => 'Str',     required => 1 );
 
+has 'parallel_processes'   => ( is => 'ro', isa => 'Int',     default => 4 );
+
 sub update
 {
   my($self) = @_;
+  my $pm = new Parallel::ForkManager($self->parallel_processes); 
   for my $species (keys %{$self->databases_attributes})
   {
+    $pm->start and next; # do the fork
     my $database = MLST::Download::Database->new(
       species => $species,
       database_attributes => $self->databases_attributes->{$species},
       base_directory      => join('/',($self->base_directory))
     );
     $database->update();
+    $pm->finish; # do the exit in the child process
   }
+  $pm->wait_all_children;
   1;
 }
 
