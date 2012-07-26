e8f0333d35756ecda208bc6702ff05f5cea5013f
andrewjpage <andrewjpage@gmail.com>
2012-07-26 17:36:10 +0100

    create a spreadsheet from multiple fasta files

--- modules/MLST/ProcessFasta.pm
+++ modules/MLST/ProcessFasta.pm
@@ -23,6 +23,7 @@ use MLST::SearchForFiles;
 use MLST::CompareAlleles;
 use MLST::SequenceType;
 use MLST::OutputFasta;
+use MLST::Spreadsheet::Row;
 
 has 'species'             => ( is => 'ro', isa => 'Str',      required => 1 ); 
 has 'base_directory'      => ( is => 'ro', isa => 'Str',      required => 1 ); 
@@ -35,6 +36,7 @@ has 'output_fasta_files'  => ( is => 'ro', isa => 'Boolean',  default  => 0 );
 has '_search_results'     => ( is => 'ro', isa => 'MLST::SearchForFiles',  lazy => 1, builder => '_build__search_results' ); 
 has '_compare_alleles'    => ( is => 'ro', isa => 'MLST::CompareAlleles',  lazy => 1, builder => '_build__compare_alleles' ); 
 has '_sequence_type_obj'  => ( is => 'ro', isa => 'MLST::SequenceType',    lazy => 1, builder => '_build__sequence_type_obj' ); 
+has '_spreadsheet_row_obj' => ( is => 'ro', isa => 'MLST::Spreadsheet::Row',    lazy => 1, builder => '_build__spreadsheet_row_obj' ); 
 
 sub _build__search_results
 {
@@ -73,6 +75,15 @@ sub _build__sequence_type_obj
   my $sequence_type_obj = MLST::SequenceType->new(
     profiles_filename => $self->_search_results->profiles_filename(),
     sequence_names    => $self->_compare_alleles->found_sequence_names
+  );
+}
+
+sub _build__spreadsheet_row_obj
+{
+  my($self) = @_;
+  MLST::Spreadsheet::Row->new(
+    sequence_type_obj => $self->_sequence_type_obj, 
+    compare_alleles   => $self->_compare_alleles
   );
 }
 
